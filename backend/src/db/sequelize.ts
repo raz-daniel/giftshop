763265ce375cd64787d3b3dbf54b1605b0b0fcca
raz-daniel <raz.daniel@gmail.com>
2025-02-27 13:11:27 +0200

    setting sequilze

--- backend/src/db/sequelize.ts
+++ backend/src/db/sequelize.ts
@@ -1,11 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import config from "config";
+import TargetMarket from "../model/target-market";
+import Present from "../model/presents";
 
 const logging = config.get<boolean>('sequelize.logging') ? console.log : false
 
 
 const sequelize = new Sequelize({
-    // models: [  ********* What Model is missing? ***************],
+    models: [  TargetMarket, Present],
     dialect: 'mysql',
     ...config.get('db'),
     logging
